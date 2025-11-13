dcebcc0fc10438a41bdb77b6b8efca14ab7eda3c
xdNecron <venomdushii@gmail.com>
2025-11-13 21:35:12 +0100

    Tweaks in error handling of cp

--- cp/custom-rec-copy-alg.c
+++ cp/custom-rec-copy-alg.c
@@ -1,23 +1,28 @@
 #define _LARGEFILE64_SOURCE
+#define _XOPEN_SOURCE 500
 
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
 
 #define BUFSIZE 1024
-
 #define C_VERBOSE 0x01
 
 int copy(char *pathname, char *dstname, int flags) {
 
   DIR *dir = opendir(pathname);
   struct dirent *entry;
+  struct stat st;
+
+  if (stat(pathname, &st) == -1) {  // derefs symlinks for now
+    perror("fstat");
+    return -1;
+  }
 
   if (!dir && errno == ENOTDIR) {
 
@@ -33,19 +38,25 @@ int copy(char *pathname, char *dstname, int flags) {
       return -1;
     }
 
-    if ((dst_fd = open(dstname, dst_flags, S_IRUSR | S_IWUSR)) == -1) {
+    if ((dst_fd = open(dstname, dst_flags, st.st_mode) == -1)) {
       perror("dst_fd");
+      close(src_fd);
       return -1;
     }
 
-    while ((bytes_read = read(src_fd, buf, BUFSIZE)) > 0) {
+
+    while ((bytes_read = read(src_fd, buf, BUFSIZE)) != 0) {
       if (bytes_read == -1) {
         perror("read");
-        break;
+        close(src_fd);
+        close(dst_fd);
+        return -1;
       }
       if (write(dst_fd, buf, bytes_read) != bytes_read) {
         perror("write");
-        break;
+        close(src_fd);
+        close(dst_fd);
+        return -1;
       }
     }
 
@@ -57,15 +68,14 @@ int copy(char *pathname, char *dstname, int flags) {
     return 0;
 
   } else {
-    if (mkdir(dstname, S_IRUSR | S_IWUSR | S_IXUSR) == -1) {
+    if (mkdir(dstname, st.st_mode) == -1) {
       perror("mkdir");
+      if (dir) closedir(dir);
       return -1;
     }
 
     if (flags & C_VERBOSE) printf("\'%s\' -> \'%s\'\n", pathname, dstname);
   }
-
-  /* errno = 0; */
 
   // while there are children to be copied
   while ((entry = readdir(dir)) != NULL) {
@@ -81,6 +91,7 @@ int copy(char *pathname, char *dstname, int flags) {
       if ((copy(new_pathname, new_dstname, flags)) == -1) {
         free(new_pathname);
         free(new_dstname);
+        closedir(dir);
         return -1;
       }
 
@@ -98,11 +109,11 @@ int main(int argc, char *argv[]) {
 
   if (argc < 3) {
     fprintf(stderr, "Usage: ./run <src> <dst>\n");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   int result = copy(argv[1], argv[2], C_VERBOSE);
-  if (result == -1) return 1;
+  if (result == -1) exit(EXIT_FAILURE);
 
   return 0;
 }
